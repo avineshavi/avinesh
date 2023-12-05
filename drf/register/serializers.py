3f96f5029a511bcebfdd27059ce362d92c8f0c71
Avinesh <avinesh7510@gmail.com>
2023-12-05 09:15:52 +0530

    drf

--- drf/register/serializers.py
+++ drf/register/serializers.py
@@ -14,7 +14,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     
         
     def validate(self, data):
-        if data['password'] != data['confirm_psasword']:
+        if data['password'] != data['confirm_password']:
             raise serializers.ValidationError('Passwords do not match')
         return data
     
